1c4aa8888560dde18fab3d790bfdf4de6b34d7a9
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-16 01:56:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (30.52%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int ans=0;
+        for(auto c: s){
+            if(c=='('){
+                st.push(c);
+            }else{
+                if(!st.empty()) st.pop();
+                else ans++;
+            }
+        }
+        return ans+st.size();
+    }
+};
