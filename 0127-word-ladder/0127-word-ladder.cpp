e242fe949ac6c54676c6af1cbd765d67862fbe3a
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-15 16:56:56 +0530

    Time: 1818 ms (5%), Space: 163.2 MB (5.01%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        map<string,bool>mp;
+        for(auto c: wordList){
+            mp[c]=true;
+        }
+        queue<pair<string,int>>rq;
+        rq.push({beginWord,1});
+        while(!rq.empty()){
+            auto c=rq.front();
+            rq.pop();
+            for(int i=0;i<c.first.length();i++){
+                for(char j='a';j<='z';j++){
+                    string s=c.first;
+                    s[i]=j;
+                    if(mp[s]&&s==endWord) return c.second+1;
+                    if(mp[s]){
+                        rq.push({s,c.second+1});
+                        mp[s]=false;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
