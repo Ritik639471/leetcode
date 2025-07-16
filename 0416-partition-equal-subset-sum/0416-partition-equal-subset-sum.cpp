8c4c931dfba24aa65b94c71a8e836062f7d69cb3
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-16 19:44:08 +0530

    Time: 648 ms (5.05%), Space: 229.1 MB (5.74%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0,n=nums.size();
+        vector<unordered_set<int>>s(n);
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2==1) return false;
+        s[0].insert(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            s[i].insert(nums[i]);
+            if(nums[i]==sum/2) return true;
+            for(auto c: s[i-1]){
+                s[i].insert(c+nums[i]);
+                if(c+nums[i]==sum/2) return true;
+            }
+            for(auto c: s[i-1]){
+                s[i].insert(c);
+            }
+        }
+        return false;
+    }
+};
