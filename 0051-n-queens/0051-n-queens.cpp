fd7871f0c8428a909cb57d50fadae41a65438d8a
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-16 12:21:15 +0530

    Time: 3 ms (65.27%), Space: 10.5 MB (39.58%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    vector<string>an;
+    bool valid(int i, int j, int n) {
+        return (i >= 0 && i < n) && (j >= 0 && j < n);
+    }
+    bool pos(int i,int j,int n){
+        for(int k=0;k<i;k++){
+            if(an[k][j]=='Q') return false;
+        }
+        for(int k=0;k<j;k++){
+            if(an[i][k]=='Q') return false;
+        }
+        for(int k=0;k<n;k++){
+            int x=i-k,y=j-k;
+            if(valid(x,y,n)&&an[x][y]=='Q') return false;
+            x=i-k;
+            y=j+k;
+            if(valid(x,y,n)&&an[x][y]=='Q') return false;
+        }
+        return true;
+    }
+    void help(int j,int n){
+        if(j==n){
+            ans.push_back(an);
+        }
+        for(int i=0;i<n;i++){
+            if(pos(j,i,n)) {
+                an[j][i]='Q';
+                help(j+1,n);
+                an[j][i]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        string str;
+        for(int i=0;i<n;i++) str+='.';
+        an=vector<string>(n,str);
+        help(0,n);
+        return ans;
+    }
+};
