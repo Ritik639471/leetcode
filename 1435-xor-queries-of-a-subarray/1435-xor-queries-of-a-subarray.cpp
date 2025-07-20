993b5ac477feb95d3ecd47d172e4213596ebed38
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-20 21:52:59 +0530

    Time: 3 ms (57.33%), Space: 90.5 MB (99.25%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -4,10 +4,9 @@ public:
         int n=arr.size();
         for(int i=1;i<n;i++) arr[i]=arr[i-1]^arr[i];
         vector<int>ans(queries.size());
-        int i=0;
-        for(auto c:queries){
-            if(c[0]==0) ans[i++]=arr[c[1]];
-            else ans[i++]=arr[c[1]]^arr[c[0]-1];
+        for(int i=0;i<queries.size();i++){
+            if(queries[i][0]==0) ans[i]=arr[queries[i][1]];
+            else ans[i]=arr[queries[i][1]]^arr[queries[i][0]-1];
         }
         return ans;
     }
