c802ea240b10f7f8dc9e529b8d27924a2af08cde
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-20 20:17:31 +0530

    Time: 20 ms (5.33%), Space: 22.8 MB (5.12%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pq) {
+        unordered_map<int,set<int>>s,s1;
+        for(int i=0;i<pq.size();i++){
+            s[pq[i][0]].insert(pq[i][1]);
+            s1[pq[i][1]].insert(pq[i][0]);
+        }
+        vector<int>ans;
+        unordered_map<int,bool>m;
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (m[i] || s[i].empty()){
+                m[i] = true;
+                q.push(i);
+                ans.push_back(i);
+            }
+        }
+        while(!q.empty()){
+            int a=q.front();
+            m[a]=true;
+            q.pop();
+            for(auto c: s1[a]){
+                s[c].erase(a);
+                if(s[c].empty()&&!m[c]){ ans.push_back(c);q.push(c);}
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(!m[i]) return {};
+        }
+        return ans;
+    }
+};
