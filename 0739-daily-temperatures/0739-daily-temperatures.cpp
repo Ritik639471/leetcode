3dd4694fcc3d1f123ab1eeb6abd7e276eda81438
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-21 20:36:48 +0530

    Time: 28 ms (35.36%), Space: 107.4 MB (24.64%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        stack<int>q;
         int n=t.size();
         vector<int>ans(n,0);
         for(int i=0;i<n;i++){
-            while(!q.empty()&&t[i]>q.top().first){
-                ans[q.top().second]=i-q.top().second;
+            while(!q.empty()&&t[i]>t[q.top()]){
+                ans[q.top()]=i-q.top();
                 q.pop();
             }
-            q.push({t[i],i});
+            q.push(i);
         }
         return ans;
     }
