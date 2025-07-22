acf5c6b887c77c61db604a49dd030ee2c54175ec
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-22 19:05:57 +0530

    Time: 893 ms (5.04%), Space: 115.3 MB (5.09%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -3,16 +3,16 @@ public:
     long long totalCost(vector<int>& costs, int k, int c) {
         int n=costs.size();
         int lo=0,hi=n-1;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p1,p2;
+        set<pair<int,int>>p1,p2;
         for(int i=0;i<c;i++){
             if(lo>hi) break;
             if(lo==hi){
-                p1.push({costs[lo],lo});
+                p1.insert({costs[lo],lo});
                 lo++;
                 break;
             }
-            p1.push({costs[lo],lo});
-            p2.push({costs[hi],hi});
+            p1.insert({costs[lo],lo});
+            p2.insert({costs[hi],hi});
             hi--;
             lo++;
         }
@@ -20,31 +20,31 @@ public:
         long long ans=0;
         for(int i=1;i<=k;i++){
             if(a==0){
-                ans+=p2.top().first;
-                p2.pop();
+                ans+=p2.begin()->first;
+                p2.erase(p2.begin());
                 b--;
             }else if(b==0){
-                ans+=p1.top().first;
-                p1.pop();
+                ans+=p1.begin()->first;
+                p1.erase(p1.begin());
                 a--;
             }else{
-                if(p2.top().first>=p1.top().first){
-                    ans+=p1.top().first;
-                    p1.pop();
+                if(p2.begin()->first>=p1.begin()->first){
+                    ans+=p1.begin()->first;
+                    p1.erase(p1.begin());
                     a--;
                 }else{
-                    ans+=p2.top().first;
-                    p2.pop();
+                    ans+=p2.begin()->first;
+                    p2.erase(p2.begin());
                     b--;
                 }
             }
             if(a<c&&lo<=hi){
-                p1.push({costs[lo],lo});
+                p1.insert({costs[lo],lo});
                 lo++;
                 a++;
             }
             if(b<c&&lo<=hi){
-                p2.push({costs[hi],hi});
+                p2.insert({costs[hi],hi});
                 hi--;
                 b++;
             }
