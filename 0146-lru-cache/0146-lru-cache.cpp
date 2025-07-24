6ae1483f08e64d85390bd6f2200a1563449f5ed6
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-24 22:45:15 +0530

    Time: 115 ms (20.53%), Space: 175 MB (57.25%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,55 @@
+class LRUCache {
+public:
+    deque<pair<int,int>>q;
+    vector<int>a;
+    vector<int>b;
+    int n,m;
+    LRUCache(int capacity) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);   
+        a=vector<int>(10005,-1);
+        b=vector<int>(10005,0);
+        n=capacity;
+        m=0;
+    }
+    
+    int get(int key) {
+        if(a[key]==-1||b[key]==0) return -1;
+        q.push_front({key,b[key]+1});
+        b[key]++;
+        cout<<m<<endl;
+        return a[key];
+    }
+    
+    void put(int key, int value) {
+        if(a[key]!=-1){
+            q.push_front({key,b[key]+1});
+            b[key]++;
+            a[key]=value;
+            return;
+        }
+        while(m==n){
+            if(a[q.back().first]==-1){
+                q.pop_back();
+            }else if(q.back().second>b[q.back().first]||q.back().second<b[q.back().first]){
+                q.pop_back();
+            }else{
+                a[q.back().first]=-1;
+                b[q.back().first]=0;
+                m-=1;
+            }
+        }
+        m++;
+        q.push_front({key,b[key]+1});
+        b[key]++;
+        a[key]=value;
+        return;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
