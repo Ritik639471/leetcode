dd9a5229dee94b8a9fed58dd4ae4d1ec168cc45c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-24 23:41:31 +0530

    Time: 887 ms (5.07%), Space: 113.7 MB (84.02%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void help(vector<int>& nums,int i,int j,int& a){
+        if(i==j) return;
+        int d=(i+j)/2;
+        help(nums,i,d,a);
+        help(nums,d+1,j,a);
+        vector<int>b(j-i+1);
+        int k=i,l=d+1;
+        while(k<=d&&l<=j){
+            if(2*(long long)nums[l]<(long long)nums[k]){
+                a+=d-k+1;
+                l++;
+            }else k++;
+        }
+        k=i;
+        l=d+1;
+        int m=0;
+        while(k<=d&&l<=j){
+            if(nums[k]<=nums[l]){
+                b[m++]=nums[k++];
+            }else{
+                b[m++]=nums[l++];
+            }
+        }
+        while(k<=d){
+            b[m++]=nums[k++];
+        }
+        while(l<=j){
+            b[m++]=nums[l++];
+        }
+        for(m=0;m<j-i+1;m++){
+            nums[i+m]=b[m];
+        }
+    }
+    int reversePairs(vector<int>& nums) {
+        int a=0;
+        int n=nums.size();
+        help(nums,0,n-1,a);
+        for(int i=0;i<n;i++) cout<<nums[i]<<endl;
+        return a;
+    }
+};
