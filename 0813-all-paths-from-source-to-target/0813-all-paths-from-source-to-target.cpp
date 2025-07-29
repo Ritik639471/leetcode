00091b4140f2881a3f9043da3b1139e5b7dbad5b
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-07-29 21:23:51 +0530

    Time: 15 ms (26.99%), Space: 22.2 MB (10.78%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void help(vector<vector<int>>& graph,int des,int s,vector<vector<int>>& ans,vector<int> v){
-        v.push_back(s);
-        if(s==des){
-            ans.push_back(v);
-        }
-        for(auto c:graph[s]){
-            help(graph,des,c,ans,v);
-        }
-    }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>>ans;
         int n=graph.size();
-        help(graph,n-1,0,ans,{});
+        vector<int>v;
+        queue<pair<int,vector<int>>>q;
+        q.push({0,v});
+        while(!q.empty()){
+            auto d=q.front();
+            q.pop();
+            d.second.push_back(d.first);
+            if(d.first==n-1){
+                ans.push_back(d.second);
+                continue;
+            }
+            for(auto c:graph[d.first]){
+                q.push({c,d.second});
+            }
+        }
         return ans;
     }
 };
