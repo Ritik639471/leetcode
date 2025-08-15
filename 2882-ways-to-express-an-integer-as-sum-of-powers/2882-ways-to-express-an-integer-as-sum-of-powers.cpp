d5f7545e484bc8f279ff16b01c069d671ea97124
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-08-15 22:09:57 +0530

    Time: 230 ms (37.39%), Space: 13 MB (71.41%) - LeetHub

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int m=1e9+7;
+    int numberOfWays(int n, int x) {
+        vector<long long>a(n+1,0);
+        a[0]=1;
+        long long ans=0;
+        for(long i=n;i>=1;i--){
+            for(int j=n-1;j>=0;j--){
+                if(a[j]&&(j+pow(i,x))<=n){
+                    a[j+pow(i,x)]=(a[j+pow(i,x)]+(a[j]))%m;
+                }
+            }
+        }
+        return a[n]%m;
+    }
+};
