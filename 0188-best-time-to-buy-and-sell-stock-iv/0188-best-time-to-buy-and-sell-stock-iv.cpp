b0f73607c4756882e0e354292d51119a3a3be633
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2025-12-16 11:32:01 +0530

    Time: 3 ms (75.57%), Space: 14.4 MB (76.9%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& A) {
+        // int n = A.size();
+        // vector<int> dp(n + 1, 0),dp1;
+        // for (int l = 1; l <= 2*k; l++) {
+        //     int prev=0;
+        //     for (int i = n - 1; i >= 0; i--) {
+        //         int cur=dp[i];
+        //         if(l%2==0){
+        //             dp[i]=max(dp[i+1],-A[i]+prev);
+        //         }
+        //         else{
+        //             dp[i]=max(dp[i+1],A[i]+prev);
+        //         }
+        //         prev=cur;
+        //         cout<<dp[i]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // return dp[0];
         int n = A.size();
-        vector<int> dp(n + 1, 0),dp1;
-        for (int l = 1; l <= 2*k; l++) {
-            int prev=0;
-            for (int i = n - 1; i >= 0; i--) {
-                int cur=dp[i];
+        vector<int> dp(2*k + 1, 0);
+        for (int i = n-1;i>=0;i--) {
+            for (int l = 1;l<=2*k;l++) {
                 if(l%2==0){
-                    dp[i]=max(dp[i+1],-A[i]+prev);
+                    dp[l]=max(dp[l],-A[i]+dp[l-1]);
                 }
                 else{
-                    dp[i]=max(dp[i+1],A[i]+prev);
+                    dp[l]=max(dp[l],A[i]+dp[l-1]);
                 }
-                prev=cur;
             }
         }
-        return dp[0];
+        return dp[2*k];
     }
 };
